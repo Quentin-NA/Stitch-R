e99595578c58dc2e75cb46f689873a5943429012
quentin natoly <quentin.natoly@gmail.com>
2020-05-26 18:53:47 +0200

    Pundit for index

---
+++ app/controllers/receipts_controller.rb
@@ -0,0 +1,9 @@
+class ReceiptsController < ApplicationController
+  
+  def index
+    # @user = getProfile
+    # @receipts = gmail.user.messages
+    @receipts = policy_scope(Receipt)
+    # authorize @receipts
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :receipts, only: [:index]
   devise_for :users, 
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end

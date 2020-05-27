90b02a80178d6004bd497adf8640002ce92fa5ca
Aurelien Rolando <aurelien.rolando@gmail.com>
2020-05-27 15:41:07 +0200

    side dash board

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :receipts, only: [:index]
-  devise_for :users, 
+  devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  get "/dashboard", to: "pages#dashboard"
 end

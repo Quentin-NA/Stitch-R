1b9316f61debbf62ef5138121d7a82e925fc3ac6
quentin natoly <quentin.natoly@gmail.com>
2020-05-28 15:43:46 +0200

    Add collection at rechercher in supplier search form

--- app/models/supplier_search.rb
+++ app/models/supplier_search.rb
@@ -1,4 +1,5 @@
 class SupplierSearch < ApplicationRecord
   CATEGORY = ['Transport', 'Alimentation','Fournitures']
+  LABEL = ['Tous les messages', 'Boîte de réception','Messages non lus', 'Messages envoyés']
   has_many :receipts
 end

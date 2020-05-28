5a6c835cc699fbeed8764553c48c7699872f60fd
quentin natoly <quentin.natoly@gmail.com>
2020-05-28 14:21:33 +0200

    Query almost done

--- app/controllers/supplier_searches_controller.rb
+++ app/controllers/supplier_searches_controller.rb
@@ -15,20 +15,37 @@ class SupplierSearchesController < ApplicationController
 
   def create
     @search = SupplierSearch.create(search_params)
+    @keyword = query(search_params)
+    authorize @search
     @user = current_user
     if @search.save
       redirect_to user_supplier_searches_path
     else
       render :new
     end
-    authorize @search
     @keyword = @search[:keyword]
-    @keyword = "#{@search[:subject]}, #{@search[:contains]}, #{@search[:not_contains]}, #{@search[:start_date]}, #{@search[:end_date]}, #{@search[:label]}, #{@search[:attachment]}"
+    @keyword = query(search_params)
+    raise
   end
-
+  
   private
 
   def search_params
     params.require(:supplier_search).permit(:from, :category, :keyword, :subject, :contains, :not_contains, :start_date, :end_date, :label, :attachment)
+  end
+
+  def query(search_params)
+    label = params[:supplier_search][:label] = "#{@search[:label]}" ? "in:#{@search[:label]} " : ""
+    email = params[:supplier_search][:from] = "#{@search[:from]}" ? "from:(#{@search[:from]}) " : ""
+    subject = params[:supplier_search][:subject] = "#{@search[:subject]}" ? "subject:(#{@search[:subject]}) " : ""
+    contains = params[:supplier_search][:contains] = "#{@search[:contains]}" ? "#{@search[:contains]} " : ""
+    not_contains = params[:supplier_search][:not_contains] = "#{@search[:not_contains]}" ? "#{@search[:not_contains]} " : ""
+    start_date = params[:supplier_search][:start_date] = "#{@search[:start_date]}" ? "after:#{@search[:start_date]} " : ""
+    end_date = params[:supplier_search][:end_date] = "#{@search[:end_date]}" ? "before:#{@search[:end_date]} " : ""
+    attachment = params[:supplier_search][:attachment] = "#{@search[:attachment]}" ? "has:attachment" : ""
+
+    start_date.gsub("-","/")
+    end_date.gsub("-","/")
+    @query_result = "#{label}#{email}#{subject}#{contains}#{not_contains}#{start_date}#{end_date}#{attachment}"
   end
 end
--- app/models/supplier_search.rb
+++ app/models/supplier_search.rb
@@ -1,4 +1,5 @@
 class SupplierSearch < ApplicationRecord
   CATEGORY = ['Transport', 'Alimentation','Fournitures']
+  LABEL = ['Tous les messages', 'Boîte de réception','Messages suivis', 'Messages envoyés', 'Messages lus', 'Messages non lus']
   has_many :receipts
 end

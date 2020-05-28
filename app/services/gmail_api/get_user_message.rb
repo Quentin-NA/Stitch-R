76f7bbf47dfe890b3342c8c9b85b9dc18b27680a
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2020-05-28 10:53:45 +0200

    cleaning

--- app/services/gmail_api/get_user_message.rb
+++ app/services/gmail_api/get_user_message.rb
@@ -3,7 +3,7 @@ class GmailApi::GetUserMessage < GmailApi::Connexion
   attr_reader :message_id
 
   def initialize(user, message_id)
-    super
+    super(user)
     @message_id = message_id
   end
 

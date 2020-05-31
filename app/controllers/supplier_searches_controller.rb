ea34a6f76972f1b5ed0bacba9b3dd00ef3bc1693
Dick Wissink <dwissink@gmail.com>
2020-05-31 02:00:33 +0200

    almost working supplier show

--- app/controllers/supplier_searches_controller.rb
+++ app/controllers/supplier_searches_controller.rb
@@ -9,11 +9,19 @@ class SupplierSearchesController < ApplicationController
     @supplier_search = SupplierSearch.find(params[:id])
     query = @supplier_search.query
     @messages = GmailApi::ListUserMessages.new(current_user).call(query)
+    @metadatas = ["from", "subject", "date"]
+    # @messages.each do |message|
+    #   headers = GmailApi::GetUserMessageHeaders.new(current_user, message.id, @metadatas).call
+    #   date = headers[0].value
+    #   from = headers[1].value
+    #   subject = headers[2].value
+    #   snippet = GmailApi::GetUserMessage.new(current_user, message.id).call.snippet
+    # end
     authorize @supplier_search
   end
 
   def new
-    @user = current_user
+    #@user = current_user
     @search = SupplierSearch.new
     authorize @search
   end
@@ -37,11 +45,22 @@ class SupplierSearchesController < ApplicationController
     @supplier_search = current_user
   end
 
-  # def count
-  #   @receipts_count =
-  # end
-
   private
+
+  # retrieving headers based on index doesn't work ... (as index of attributes varies per supplier)
+    # @date = @message.payload.headers[15].value
+    # @from = @message.payload.headers[16].value
+    # @subject = @message.payload.headers[20].value
+    # @snippet = @message.snippet
+
+  def headers_to_hash
+    @headers = []
+    @header_obj.each do |header|
+      @label = header.name
+      @content = header.value
+      @header_content << @content #"{#{@label}"=>" + #{@content}}"
+    end
+  end
 
   def search_params
     params.require(:supplier_search).permit(:from, :category, :keyword, :subject, :contains, :not_contains, :start_date, :end_date, :label, :attachment)

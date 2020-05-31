690ceb479e588a20475adc6b79d257d2d600f9b0
Dick Wissink <dwissink@gmail.com>
2020-05-31 18:37:25 +0200

    back to previous version + minor tweaks

--- app/controllers/supplier_searches_controller.rb
+++ app/controllers/supplier_searches_controller.rb
@@ -9,14 +9,10 @@ class SupplierSearchesController < ApplicationController
     @supplier_search = SupplierSearch.find(params[:id])
     query = @supplier_search.query
     @messages = GmailApi::ListUserMessages.new(current_user).call(query)
-    @metadatas = ["from", "subject", "date"]
-    # @messages.each do |message|
-    #   headers = GmailApi::GetUserMessageHeaders.new(current_user, message.id, @metadatas).call
-    #   date = headers[0].value
-    #   from = headers[1].value
-    #   subject = headers[2].value
-    #   snippet = GmailApi::GetUserMessage.new(current_user, message.id).call.snippet
-    # end
+    @metadatas = ["subject", "date"]
+    # @message_headers = GmailApi::GetUserMessageHeaders.new(current_user, message.id, @metadatas)
+    # this should be an array of hashes message_id=>(key=>value) for each @metadata
+    # the view should iterate on these + GetMessage.call.snippet
     authorize @supplier_search
   end
 
@@ -36,9 +32,6 @@ class SupplierSearchesController < ApplicationController
     else
       render :new
     end
-
-    # @keyword = @search[:keyword]
-    # @keyword = "#{@search[:subject]}, #{@search[:contains]}, #{@search[:not_contains]}, #{@search[:start_date]}, #{@search[:end_date]}, #{@search[:label]}, #{@search[:attachment]}"
   end
 
   def supplier_id
@@ -47,22 +40,38 @@ class SupplierSearchesController < ApplicationController
 
   private
 
+  def search_params
+    params.require(:supplier_search).permit(:from, :category, :keyword, :subject, :contains, :not_contains, :start_date, :end_date, :label, :attachment)
+  end
+end
+
+  # Trials & errors to retrieve headers =
+
   # retrieving headers based on index doesn't work ... (as index of attributes varies per supplier)
     # @date = @message.payload.headers[15].value
     # @from = @message.payload.headers[16].value
     # @subject = @message.payload.headers[20].value
     # @snippet = @message.snippet
+  # = Try to build a @label=>@value hash per header ...
 
-  def headers_to_hash
-    @headers = []
-    @header_obj.each do |header|
-      @label = header.name
-      @content = header.value
-      @header_content << @content #"{#{@label}"=>" + #{@content}}"
-    end
-  end
+    # @messages.each do |message|
+    #   headers = GmailApi::GetUserMessageHeaders.new(current_user, message.id, @metadatas).call
+    #   date = headers[0].value
+    #   from = headers[1].value
+    #   subject = headers[2].value
+    #   snippet = GmailApi::GetUserMessage.new(current_user, message.id).call.snippet
+    # end
 
-  def search_params
-    params.require(:supplier_search).permit(:from, :category, :keyword, :subject, :contains, :not_contains, :start_date, :end_date, :label, :attachment)
-  end
-end
+  # def headers_to_hash
+    # @header_objects = []
+    # @header_objects.each do |header|
+    #   @label = header.name
+    #   @content = header.value
+    #   @header_hashed = "{'#{@label}'"+"=>"+"'#{@value}'}"
+    #   @headers_hashed << @header_hashed
+    # end
+
+    # @header_hashed = "#{@label}:'#{@value}'"
+
+    # should create an array of x metadata hashes
+

d125e7a7eae562c7e440532f566f3eec81e9c6f7
quentin natoly <quentin.natoly@gmail.com>
2020-06-01 12:03:09 +0200

    select row function en js

--- app/models/supplier_search.rb
+++ app/models/supplier_search.rb
@@ -12,8 +12,8 @@ class SupplierSearch < ApplicationRecord
     query << "subject:(#{subject}) " if subject.present?
     query << "#{contains} " if contains.present?
     query << "#{not_contains} " if not_contains.present?
-    query << "after:#{start_date.strftime('%Y/%m/%d')} " if !start_date.today?
-    query << "before:#{end_date.strftime('%Y/%m/%d')} " if !end_date.today?
+    # query << "after:#{start_date.strftime('%Y/%m/%d')} " if !start_date.today?
+    # query << "before:#{end_date.strftime('%Y/%m/%d')} " if !end_date.today?
     query << "has:attachment" if attachment.present?
 
     query.join

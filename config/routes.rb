ad410fa5c862ee9a1a9c3d4646a7739177c2ab6f
Aurelien Rolando <aurelien.rolando@gmail.com>
2020-06-01 11:54:30 +0200

    delete on receivers

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index]
   resources :receipts, only: [:index]
-  resources :receivers, only: [:index, :new, :create]
+  resources :receivers, only: [:index, :new, :create, :destroy]
   resources :supplier_searches, only: [:index, :show, :new, :create]
 
   get "/dashboard", to: "pages#dashboard"

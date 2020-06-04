0b1d72c864b98ddd3d2b29b4780b532349810754
Dick Wissink <dwissink@gmail.com>
2020-06-04 12:17:07 +0200

    receiver&seed fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ a = SupplierSearch.create(
   from: "uber.france@uber.com",
   contains: "'votre course'",
   not_contains: "Jump",
-  image: "logo/uber.png"
+  image: "logos/uber.png",
   start_date: "01/01/2018"
   )
 # => https://lh3.googleusercontent.com/qy_wZ92sFQccojEtscg52vtdAQmCIeQ4jsybMPmuML9Or7_SEOyrt0Jn0wyG-l2Fyw=s360-rw
@@ -17,7 +17,7 @@ b = SupplierSearch.create(
   name: "Oui-SNCF",
   image: "https://lh3.googleusercontent.com/1SOSCf--cpvBQPBx3hlfMdWXpQfvlWwHoxg6F5aTI8WVTzRudsA7YeohiDkH7WsyRw=s360-rw",
   from: "noreply@oui.sncf",
-  attachment: true
+  attachment: true,
   start_date: "01/01/2019"
   )
 
@@ -25,7 +25,7 @@ c = SupplierSearch.create(
   name: "Trainline",
   image: "https://lh3.googleusercontent.com/01DZthgdocCF5tYXt3MxpV_zTxXV1CST6U9Fbsr9SXygqw7dD42YjlZMAxUO6N4-VZI=s360-rw",
   from: "guichet@trainline.fr",
-  attachment: true
+  attachment: true,
   start_date: "01/01/2019"
   )
 # => https://lh3.googleusercontent.com/01DZthgdocCF5tYXt3MxpV_zTxXV1CST6U9Fbsr9SXygqw7dD42YjlZMAxUO6N4-VZI=s360-rw
@@ -34,7 +34,7 @@ d = SupplierSearch.create(
   name: "Free Mobile",
   image: "https://lh3.googleusercontent.com/h3CRflIF-NE--cL35GXcNCJzk2PYV4_NO1XxRFifnKUKfnGuMwAtcY8Vs6YQE4yhY5I=s360-rw",
   from: "freemobile@free-mobile.fr",
-  attachment: true
+  attachment: true,
   start_date: "01/01/2019"
   )
 
@@ -51,29 +51,29 @@ f = SupplierSearch.create(
   name: "Airbnb",
   image: "https://lh3.googleusercontent.com/BQnvuZR500pg2ulvv3FBmRI93ODz3AjNfbz92hCieuJLvmbGY36AKhETMTTfTDgpPQI=s360-rw",
   from: "express@airbnb.com",
-  contains: "reçu"
+  contains: "reçu",
   start_date: "01/01/2019"
   )
 
 g = SupplierSearch.create(
   name: "Booking",
-  image: "logo/booking.png",
+  image: "logos/booking.png",
   from: "customer.service@booking.com",
-  contains: "confirmée"
+  contains: "confirmée",
   start_date: "01/01/2019"
   )
 # => https://lh3.googleusercontent.com/A0QsKezU_em5H1IiwKUUluEDOK7VuGVNLii3FVL89NDVKVvRsOSHjS5AeLAUywYhArE3=s360-rw
 
 h = SupplierSearch.create(
   name: "Easyjet",
-  image: "logo/easyjet.png",
-  from: "confirmation@easyjet.com"
+  image: "logos/easyjet.png",
+  from: "confirmation@easyjet.com",
   start_date: "01/01/2019"
   )
 
 i = SupplierSearch.create(
   name: "Uber Eats",
-  image: "logo/uber_eats.png",
+  image: "logos/uber_eats.png",
   from: "uber.france@uber.com",
   subject: "eats",
   not_contains: "pourboire",
@@ -83,17 +83,17 @@ i = SupplierSearch.create(
 
 j = SupplierSearch.create(
   name: "Uber Jump",
-  image: "logo/jump.png",
+  image: "logos/jump.png",
   from: "uber.france@uber.com",
-  subject: "Jump"
+  subject: "Jump",
   start_date: "01/01/2019"
   )
 
 k = SupplierSearch.create(
   name: "Lime",
-  image: "logo/lime.png",
+  image: "logos/lime.png",
   from: "no-reply@limebike.com",
-  subject: "reçu"
+  subject: "reçu",
   start_date: "01/01/2019"
   )
 
@@ -101,13 +101,14 @@ l = SupplierSearch.create(
   name: "Oui-go",
   image: "https://lh3.googleusercontent.com/01DZthgdocCF5tYXt3MxpV_zTxXV1CST6U9Fbsr9SXygqw7dD42YjlZMAxUO6N4-VZI=s360-rw",
   from: "ne-pas-repondre@pasngr.ouigo.com",
-  attachment: true
+  attachment: true,
   start_date: "01/01/2019"
   )
 
 m = SupplierSearch.create(
   name: "Taxi G7",
-  image: "no-reply@g7.fr",
+  from: "no-reply@g7.fr",
+  image: "logos/taxiG7.png",
   subject: "votre course",
   start_date: "01/01/2019"
   )
@@ -115,7 +116,6 @@ m = SupplierSearch.create(
 
 
 puts 'SupplierSearch Created'
-
 
 
 # SupplierSearchesUser.create(supplier_search: a, user: User.first)
